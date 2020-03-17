0496c9f6a7e164b44b665f94495da2b376bf68bc
Alexis <alexis@incogito.org>
2020-03-17 19:01:13 +0100

    - Updated: ChangeLogs - Updated: Packages

--- src/Core/SuperMemoAssistant.Core/Properties/AssemblyInfo.cs
+++ src/Core/SuperMemoAssistant.Core/Properties/AssemblyInfo.cs
@@ -65,7 +65,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.3.267")]
-[assembly: AssemblyFileVersion("2.0.3.267")]
+[assembly: AssemblyVersion("2.0.3.276")]
+[assembly: AssemblyFileVersion("2.0.3.276")]
 
 [assembly: AssemblyInformationalVersion("2.0.3")]

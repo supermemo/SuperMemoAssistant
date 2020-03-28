2472f009c343b4183166354523afeb5ec598a5d1
Alexis <alexis@incogito.org>
2020-03-28 22:22:28 +0100

    - Removed: SuperMemoAssistant.Interop (replacing with submodule) - Removed: SuperMemoAssistant.Services.HTML (replacing with submodule) - Removed: SuperMemoAssistant.Services.Medias.Images (replacing with submodule) - Removed: SuperMemoAssistant.Services.Sentry (replacing with submodule) - Removed: SuperMemoAssistant.Services.UI (replacing with submodule)

--- src/Core/SuperMemoAssistant.Interop/Interop/SuperMemo/Registry/Types/ITemplateRegistry.cs
+++
@@ -1,8 +0,0 @@
-﻿using SuperMemoAssistant.Interop.SuperMemo.Registry.Members;
-
-namespace SuperMemoAssistant.Interop.SuperMemo.Registry.Types
-{
-  public interface ITemplateRegistry : IRegistry<ITemplate>
-  {
-  }
-}
